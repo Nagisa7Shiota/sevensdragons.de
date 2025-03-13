6a55bd91659a4311de1cb85d284bf168267580b9
Nagisa7Shiota <121880460+Nagisa7Shiota@users.noreply.github.com>
2025-03-13 15:50:05 +0100

    Update
    - Navbar begin.

---
+++ src/components/icons/ComponentNavbar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router';
+  import '../../assets/comnav.css';
+</script>
+
+<template>
+
+  <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/legal">Legal</RouterLink>
+  </nav>
+
+</template>
+
