83f8bf621c74f48870c4099b48727fed2a3c71e7
Nagisa7Shiota <121880460+Nagisa7Shiota@users.noreply.github.com>
2025-03-20 16:55:48 +0100

    Update
    - CSS added.
    - Gonna design it better but works!

--- src/components/icons/ComponentNavbar.vue
+++ src/components/icons/ComponentNavbar.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-  import { RouterLink } from 'vue-router';
-  import '../../assets/comnav.css';
+import { RouterLink } from 'vue-router';
+import '../../assets/comnav.css'; // Ensure this path is correct
 </script>
 
 <template>
   <nav>
-    <RouterLink label="Home" to="/">Home</RouterLink>
-    <RouterLink label="About" to="/about">About</RouterLink>
-    <RouterLink label="Legal" to="/legal">Legal</RouterLink>
+    <RouterLink class="nav-component" to="/" exact-active-class="is-active">Home</RouterLink>
+    <RouterLink class="nav-component" to="/about" exact-active-class="is-active">About</RouterLink>
+    <RouterLink class="nav-component" to="/legal" exact-active-class="is-active">Legal</RouterLink>
   </nav>
 </template>
